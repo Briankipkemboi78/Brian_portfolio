f41348e8e39cb401b9d7da98c18697c32d9bff8b
Brian <bkipkemboi@watuafrica.co.ke>
2023-09-25 15:57:10 +0300

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,37 @@
+// menu icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+//scroll sections
+
+window.onscroll = () => {
+   
+    //sticky  header
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
+
+
+/*========Scroll reveal ======*/
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' })
+ScrollReveal().reveal('.home-img img', { origin: 'bottom' })
+
+
+
